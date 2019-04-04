9c34bbce87811eb27481a360c348cffb4f7a0385
Abimzy <eabiemo@gmail.com>
2019-04-04 16:23:02 -0700

    Add route for landing page

--- app.py
+++ app.py
@@ -5,5 +5,12 @@ app = Flask(__name__)
 def index():
     return render_template('index.html', title='Home', )
 
+
+
+@app.route('/landing')
+def landing():
+    return render_template('landing.html', title="Home" )
+
+
 if __name__ == '__main__':
     app.run(debug=True)
