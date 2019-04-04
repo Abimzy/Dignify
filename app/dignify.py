d7779cc80137ff273c2437f912978b1916e17cc5
Abimzy <eabiemo@gmail.com>
2019-04-03 20:06:11 -0700

    Update index route, add static folder & clean up workspace

--- app/dignify.py
+++
@@ -1,3 +0,0 @@
-#MAIN APPLICATION MODULE
-
-from app import app
--- app/routes.py
+++
@@ -1,8 +0,0 @@
-#View functions below map to one or more URLS so Flask knows what logic to implement when a client gives or goes to a given URL
-
-from app import app
-
-@app.route('/')
-@app.route('/index')
-def index():
-    return "What else can we display here! Now let the app devt BEGIN!"
