551d9b67df7f683c36dc33e495c2aeba2059043a
Abimzy <eabiemo@gmail.com>
2019-04-05 12:32:53 -0700

    Create about route and template

--- app.py
+++ app.py
@@ -10,7 +10,11 @@ app.secret_key = 'uyghbfedivjnfecsvohldfnsjhln'
 @app.route('/')
 @app.route('/home')
 def home():
-    return render_template('home.html', title="Home" )
+    return render_template('home.html', title="Home")
+
+@app.route('/about')
+def about():
+    return render_template('about.html', title="About")
 
 
 if __name__ == '__main__':
