7edc77a7e3c505fadc8e40045f3d71f2411819bd
Abimzy <eabiemo@gmail.com>
2019-04-06 13:42:18 -0700

    Add User form

---
+++ forms.py
@@ -0,0 +1,17 @@
+
+from flask_wtf import FlaskForm as Form
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextField, TextAreaField, FileField, IntegerField, HiddenField, SelectField
+
+# import User model
+from models import User
+
+# create the class and variables to house Field definitions
+class UserForm(Form):
+    id = IntegerField()
+    first_name = TextField("First Name")
+    last_name = TextField("Last Name")
+    email = TextField("Email Address")
+    password = TextField("Password")
+    role = TextField()
+    avatar = StringField()
+    submit = SubmitField("Update")
